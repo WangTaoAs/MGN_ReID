7701a501a015d4516842901e1c8f8de7664be743
WangTaoAs <467710806@qq.com>
2021-10-11 19:39:36 +0800

    update config

--- configs.py
+++ configs.py
@@ -2,7 +2,7 @@ import argparse
 
 parser = argparse.ArgumentParser(description='MGN')
 
-parser.add_argument("--cuda", type=int, default=3, help='dataset directory')
+parser.add_argument("--cuda", type=int, default=2, help='dataset directory')
 
 # Data
 parser.add_argument("--root", type=str, default="/home/wangt/mgn_master/dataset", help='dataset directory')
@@ -20,6 +20,8 @@ parser.add_argument("--padding", type=int, default=10, help='')
 
 parser.add_argument('--feats', type=int, default=256, help='number of feature maps')
 parser.add_argument('--num_classes', type=int, default=751, help='')
+# parser.add_argument('--num_classes', type=int, default=751, help='')
+
 
 # train
 parser.add_argument("--epochs", type=int, default=80, help='number of epochs to train')
@@ -30,22 +32,33 @@ parser.add_argument("--batchtest", type=int, default=64, help='input batch size 
 parser.add_argument("--num_worker", type=int, default=8, help='input batch size for test')
 parser.add_argument("--label_smooth",type=str, default='on', help='input batch size for test')
 
+# test
+
+parser.add_argument('--test_weight', type=str, default='/home/wangt/mgn_master/MGN_80.pth')
 
 
 # optim
 
-parser.add_argument('--optimizer', default='SGD', help='')
+parser.add_argument('--optimizer', default='ADAM', help='')
 parser.add_argument('--momentum', type=float, default=0.9, help='')
 parser.add_argument('--dampening',type=float, default=0, help='')
 parser.add_argument('--nesterov', action='store_true', help='')
-parser.add_argument('--lr', type=float, default=0.001, help='')
+parser.add_argument('--lr', type=float, default=2e-4, help='')
 parser.add_argument('--gamma', type=float, default=0.1, help='')
 parser.add_argument('--lr_decay', type=int, default=60, help='')
 parser.add_argument('--weight_decay', type=float, default=5e-4, help='')
 parser.add_argument('--decay_type', type=str, default='step', help='')
 parser.add_argument('--no_margin', default= True , help='')
+parser.add_argument('--beta1', default= 0.9 , help='')
+parser.add_argument('--beta2', default= 0.999 , help='')
+parser.add_argument('--epsilon', default= 1e-8 , help='')
+parser.add_argument('--amsgrad', action='store_true', help='')
+
+
+
+
     # if no_margin is False then margin works 
-parser.add_argument('--margin',type=float, default=0.3 , help='')
+parser.add_argument('--margin',type=float, default=1.2 , help='')
 
 
 #log
