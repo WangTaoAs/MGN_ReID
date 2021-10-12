f2d081d196b8bc5dbf78d4b2c491c16f9fbce2e6
WangTaoAs <467710806@qq.com>
2021-10-12 10:41:01 +0800

    update train.py

--- train.py
+++ train.py
@@ -17,18 +17,31 @@ def set_seed(seed):
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = True
 
+
+def load_pretrain(model, pretrain=True, ppath="/home/wangt/.cache/torch/checkpoints/resnet50-19c8e357.pth", cpu=False):
+    if cpu:
+        kwargs = {'map_location': lambda storage, loc: storage}
+    else:
+        kwargs = {}
+    
+    if pretrain != False:
+        print('Load pre-trained model from {}'.format(ppath))
+        model.load_state_dict(torch.load(ppath, **kwargs), strict=False)
+
 def train(args):
     
     output_dir = args.output_dir
     if output_dir and not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
-    train_loader, val_loader, class_num, cam_num, view_num = make_data_loader(args)
+    train_loader, val_loader, class_num, _, _, num_query = make_data_loader(args)
 
     if class_num != args.num_classes:
         raise Exception('The number of dataset classes is not equal to the model')
 
     model = bulid_MGN_resnet(args)
+
+    load_pretrain(model, cpu=True)
 
     tri_loss, soft_loss = make_loss(args, class_num)
     
@@ -38,7 +51,7 @@ def train(args):
     
     scheduler = make_scheduler(args, optimizer)
 
-    Trainer(args, model, train_loader, val_loader, optimizer, scheduler, loss_fn)
+    Trainer(args, model, train_loader, val_loader, optimizer, scheduler, loss_fn, num_query)
 
 if __name__ == '__main__':
     
